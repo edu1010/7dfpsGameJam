85ae1d82090e4795b29442b0759ba035c95d71a2
Lidia <lalvarezc@edu.tecnocampus.cat>
2021-12-15 16:39:08 +0100

    Change TutoLevel

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -89,6 +89,7 @@ public class Player : MonoBehaviour, IRestartGameElements
 
     void Awake()
     {
+        Application.targetFrameRate = 60;
         m_Yaw = transform.rotation.eulerAngles.y;
         m_Pitch = m_PitchControllerTransform.localRotation.eulerAngles.x;
         m_CharacterController = GetComponent<CharacterController>();
