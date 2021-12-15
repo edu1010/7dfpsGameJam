97c726127333e3e37cfdcbf54f7bef49d25ca552
edu1010 <39130123+edu1010@users.noreply.github.com>
2021-12-16 00:26:39 +0100

    moverObjetos

---
+++ Assets/Scripts/MoveObjects.cs
@@ -0,0 +1,24 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjects : MonoBehaviour
+{
+    public List<Transform> m_WayyPoints;
+    int m_CurrentWaypoint = 0;
+    public float m_Speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
