8a6c2914ef884a3e6a3fe6df32f800c329306a14
edu1010 <39130123+edu1010@users.noreply.github.com>
2021-12-16 01:11:16 +0100

    Moves obstacles

--- Assets/Scripts/MoveObjects.cs
+++ Assets/Scripts/MoveObjects.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class MoveObjects : MonoBehaviour
 {
-    public List<Transform> m_WayyPoints;
+    public List <Transform> m_WayyPoints;
     int m_CurrentWaypoint = 0;
-    public float m_Speed = 20;
+    public float m_Time = 20;
+    float m_Smooth = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,18 @@ public class MoveObjects : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float l_rateTiempo = 1f / m_Time;
+        transform.position = Vector3.Lerp(transform.position, m_WayyPoints[m_CurrentWaypoint].position, l_rateTiempo);
+            if(Vector3.Distance(transform.position, m_WayyPoints[m_CurrentWaypoint].position) <= 1)
+        {
+            m_CurrentWaypoint += 1;
+            if(m_CurrentWaypoint >= m_WayyPoints.Count)
+            {
+                m_CurrentWaypoint = 0;
+            }
+        }
     }
 
 
